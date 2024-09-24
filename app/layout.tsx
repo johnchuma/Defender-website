6750e00f10b3d11f24133c1cdf8d18db7d1db7c9
johnvchuma <john@smartfoundry.co>
2024-09-24 08:12:20 +0300

    push className

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,32 +5,32 @@ import "./globals.css";
 const Gilroy = localFont({
   src: [
     {
-      path: './fonts/Gilroy-Bold.ttf',
-      weight: '600',
-      style: 'normal',
+      path: "./fonts/Gilroy-Bold.ttf",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: './fonts/Gilroy-Heavy.ttf',
-      weight: '700',
-      style: 'normal',
+      path: "./fonts/Gilroy-Heavy.ttf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: './fonts/Gilroy-Light.ttf',
-      weight: '200',
-      style: 'normal',
+      path: "./fonts/Gilroy-Light.ttf",
+      weight: "200",
+      style: "normal",
     },
     {
-      path: './fonts/Gilroy-Medium.ttf',
-      weight: '300',
-      style: 'normal',
+      path: "./fonts/Gilroy-Medium.ttf",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: './fonts/Gilroy-Regular.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/Gilroy-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
   ],
-})
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -44,10 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={Gilroy.className}
-        {children}
-      </body>
+      <body className={Gilroy.className}>{children}</body>
     </html>
   );
 }
