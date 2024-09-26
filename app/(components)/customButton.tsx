b9b8022c15fc714f4b5f89a640bb1ddac80e3d8f
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2024-09-27 02:19:54 +0300

    style: updated button to accept classNames and correct handling of cn

--- app/(components)/customButton.tsx
+++ app/(components)/customButton.tsx
@@ -1,23 +1,33 @@
 import React from "react";
+import { cn } from "@/app/lib/utils";
 
 interface BtnItem {
   btntext: string;
   width?: string;
   onClick?: () => void;
+  className?: string;
 }
 
-export default function CustomButton({ btntext, width, onClick }: BtnItem) {
+export default function CustomButton({
+  btntext,
+  width,
+  onClick,
+  className,
+}: BtnItem) {
   const handleClick = () => {
     if (onClick) {
       onClick();
     }
   };
+
   return (
     <div>
       <button
-        className={`mt-4 bg-primaryColor text-white text-xs py-3 px-20 rounded transition-all hover:shadow-primaryColor hover:ease-linear hover:duration-300 hover:shadow-lg hover:outline-primaryColor hover:outline-offset-8 hover:border-white hover:ring-2 hover:ring-primaryColor ${
-          width ? width : "w-auto"
-        }`}
+        className={cn(
+          `rounded bg-primaryColor px-20 py-3 text-xs text-white transition-all hover:border-white hover:shadow-lg hover:shadow-primaryColor hover:outline-offset-8 hover:outline-primaryColor hover:ring-2 hover:ring-primaryColor hover:duration-300 hover:ease-linear`,
+          width ? width : "w-auto",
+          className,
+        )}
         onClick={handleClick}
       >
         {btntext}
