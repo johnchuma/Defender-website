b6a86558201d48985bced70e649d2ac13ac2d3a7
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2024-09-29 03:17:15 +0300

    feat: landing page responsiveness

--- app/_components/title.tsx
+++ app/_components/title.tsx
@@ -9,7 +9,10 @@ interface TitleProps {
 const Title: React.FC<TitleProps> = ({ children, className }) => {
   return (
     <div
-      className={cn("text-center text-3xl font-semibold capitalize", className)}
+      className={cn(
+        "text-center text-2xl font-semibold capitalize md:text-3xl",
+        className,
+      )}
     >
       {children}
     </div>
