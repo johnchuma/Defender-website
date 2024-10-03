6d35918731f8cb05a60a838937eb4871eb622e86
diane <diane@smartfoundry.co>
2024-10-03 20:56:29 +0300

    style:contact page mobile view

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,17 +3,17 @@ import ContactInfo from "../_components/contact-info";
 
 export default function Contact() {
     return (
-        <div className="w-9/12 mx-auto space-y-20 pb-20">
+        <div className="w-11/12 md:w-9/12 mx-auto space-y-20 pb-20">
             <div className="">
             <h4 className="text-center font-semibold text-black text-3xl">
             Contact Us
           </h4>
             </div>
-            <div className="flex items-center space-x-10">
-              <div className="w-6/12">
+            <div className="flex md:flex-row flex-col items-center space-x-10">
+              <div className="w-full md:w-6/12">
                 <ContactUs />
               </div>
-              <div className="w-6/12">
+              <div className="w-full md:w-6/12">
                 <ContactInfo />
               </div>
             </div>
