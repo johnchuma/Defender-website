4497ad54dc3b3c4f519b01d8b44f9ce2e58936d1
johnvchuma <john@smartfoundry.co>
2024-10-17 18:44:48 +0300

    fix: restrict caching

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.cache = false; // Disable Webpack cache
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
-        protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL || 'https',
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || 'example.com',
-        port: process.env.NEXT_PUBLIC_IMAGE_PORT || '', 
-        pathname: process.env.NEXT_PUBLIC_IMAGE_PATHNAME || '/images/*',
+        protocol: process.env.NEXT_PUBLIC_IMAGE_PROTOCOL || "https",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME || "example.com",
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT || "",
+        pathname: process.env.NEXT_PUBLIC_IMAGE_PATHNAME || "/images/*",
       },
     ],
   },
